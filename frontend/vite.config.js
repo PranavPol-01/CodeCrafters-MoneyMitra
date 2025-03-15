b1e9c178d34ba9a0725bb47ee8aaab6ea83d5b17
PranavPol-01 <pranav.s.pol144@gmail.com>
2025-03-15 15:31:04 +0530

    tailwind and shadcn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc' // <-- Change this line
+import tailwindcss from '@tailwindcss/vite';
+import path from "path";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(), // <-- Now using Babel instead of SWC
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
