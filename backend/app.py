d15bed21ca9d27ba5ae9c60695a56054e020b7e5
Aadil Shah <adilns786@gmail.com>
2025-03-16 03:15:33 +0530

    deployinng

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,13 @@ from routes.auth_routes import auth_routes
 from routes.investment_routes import investment_bp
 from routes.transaction_routes import transaction_routes
 from routes.user_routes import user_routes
+import os
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+
+@app.route('/')
+def home():
+    return "Hello, this is moneymitra api!"
 
 app.register_blueprint(auth_routes, url_prefix='/auth')
 app.register_blueprint(investment_bp)
@@ -14,5 +19,7 @@ app.register_blueprint(transaction_routes)
 app.register_blueprint(stock_routes)
 app.register_blueprint(user_routes)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render provides PORT env variable
+    app.run(host='0.0.0.0', port=port)
