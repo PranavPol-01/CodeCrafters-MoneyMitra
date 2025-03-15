791194ea7cbdc04f893c46061ad9e4a29cb57ef8
Darshan Khapekar <darshankhapekar8520@gmail.com>
2025-03-15 17:30:47 +0530

    Add and Remove Investment Done

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.auth_routes import auth_routes
+from routes.investment_routes import investment_bp
 
 app = Flask(__name__)
 app.register_blueprint(auth_routes, url_prefix='/auth')
+app.register_blueprint(investment_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
